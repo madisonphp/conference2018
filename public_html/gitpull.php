e68183f5738668edf66417c08433411fefd13853
Andrew Shell <andrew@andrewshell.org>
2018-07-25 11:00:48 -0500

    Update gitpull.php

--- public_html/gitpull.php
+++ public_html/gitpull.php
@@ -6,3 +6,6 @@ header("Content-type: text/plain"); // be explicit to avoid accidental XSS
 $gitpath = '/usr/bin/git';
 chdir(__DIR__ . '/../'); // rarely actually an acceptable thing to do
 system("/usr/bin/env -i {$gitpath} pull 2>&1"); // main repo (current branch)
+
+$composer = '/usr/local/bin/composer';
+system("/usr/bin/env -i {$composer} install 2>&1");
